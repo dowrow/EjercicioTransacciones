bfd6b8f2d4b5dfe0380401a64bdb93cc73e8b3d2
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2018-06-24 23:50:04 +0200

    Extend transaction service specs

--- client/src/app/services/transaction.service.spec.ts
+++ client/src/app/services/transaction.service.spec.ts
@@ -1,5 +1,6 @@
 import { asyncData } from "../testing/helpers";
 import { TransactionService } from "./transaction.service";
+import { Transaction } from "../models/transaction";
 
 describe("TransactionService", () => {
   let httpClientSpy: { get: jasmine.Spy };
@@ -99,38 +100,129 @@ describe("TransactionService", () => {
     expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
   });
 
-  it("should get 2 transactions without location", () => {
-    const expectedResponse = {
-      transactions: [
-        {
-          transaction_id: "y9sfpmzj3fcijba7v6iqf47vi",
-          money_amount: "6588€",
-          user_id: "ajy62goz3onyabby1azv3g14i",
-          courier_id: "lzd6vk0zibxp5aw3tyb9",
-          new_user: "no",
-          created_at: "2016-01-04T17:08:25.473Z"
-        },
-        {
-          transaction_id: "z9sfpmzj3fcijba7v6iqf47vk",
-          money_amount: "4488€",
-          user_id: "ajy62goz3onyabby1azv3g14i",
-          courier_id: "lzd6vk0zibxp5aw3tyb9",
-          new_user: "no",
-          created_at: "2016-01-03T17:08:35.473Z"
-        }
-      ]
+  it("should parse server transaction without location", () => {
+    const serverTransaction = {
+      transaction_id: "y9sfpmzj3fcijba7v6iqf47vi",
+      money_amount: "6588€",
+      user_id: "ajy62goz3onyabby1azv3g14i",
+      courier_id: "lzd6vk0zibxp5aw3tyb9",
+      new_user: "no",
+      created_at: "2016-01-04T17:08:25.473Z"
     };
-    httpClientSpy.get.and.returnValue(asyncData(expectedResponse));
-    service
-      .getTransactions(0, 2, false)
-      .subscribe(
-        transactions =>
-          expect(transactions).toEqual(
-            expectedResponse,
-            "expected transactions"
-          ),
-        fail
-      );
-    expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
+    const transaction: Transaction = TransactionService.parseServerTransaction(
+      serverTransaction
+    );
+    expect(transaction.transactionId).toBe(serverTransaction.transaction_id);
+    expect(transaction.moneyAmount).toBe(6588);
+    expect(transaction.userId).toBe(serverTransaction.user_id);
+    expect(transaction.courierId).toBe(serverTransaction.courier_id);
+    expect(transaction.isNewUser).toBeFalsy();
+    expect(+transaction.createdAt).toBe(
+      +new Date(serverTransaction.created_at)
+    );
+    expect(transaction.destination).toBeUndefined();
+    expect(transaction.origin).toBeUndefined();
+  });
+
+  it("should parse server transaction with location", () => {
+    const serverTransaction = {
+      transaction_id: "y9sfpmzj3fcijba7v6iqf47vi",
+      money_amount: "6588€",
+      user_id: "ajy62goz3onyabby1azv3g14i",
+      courier_id: "lzd6vk0zibxp5aw3tyb9",
+      new_user: "no",
+      created_at: "2016-01-04T17:08:25.473Z",
+      origin: {
+        id: "USA",
+        description: "-",
+        lat: "0.1",
+        lng: "-0.1"
+      },
+      destination: {
+        id: "ESP",
+        description: "-",
+        lat: "0.1",
+        lng: "-0.1"
+      }
+    };
+    const transaction: Transaction = TransactionService.parseServerTransaction(
+      serverTransaction
+    );
+    expect(transaction.transactionId).toBe(serverTransaction.transaction_id);
+    expect(transaction.moneyAmount).toBe(6588);
+    expect(transaction.userId).toBe(serverTransaction.user_id);
+    expect(transaction.courierId).toBe(serverTransaction.courier_id);
+    expect(transaction.isNewUser).toBeFalsy();
+    expect(+transaction.createdAt).toBe(
+      +new Date(serverTransaction.created_at)
+    );
+    expect(transaction.destination).toBeDefined();
+    expect(transaction.destination.lat).toBe(0.1);
+    expect(transaction.destination.lng).toBe(-0.1);
+    expect(transaction.origin).toBeDefined();
+    expect(transaction.destination.lat).toBe(0.1);
+    expect(transaction.destination.lng).toBe(-0.1);
+  });
+
+  it("should check if needs special documentation", () => {
+    const undocumentedTransaction: Transaction = {
+      origin: {
+        id: "ESP",
+        lat: 1,
+        lng: 0,
+        description: "-"
+      },
+      destination: {
+        id: "USA",
+        lat: 1,
+        lng: 0,
+        description: "-"
+      },
+      transactionId: "transaction-id",
+      moneyAmount: 1000,
+      userId: "user-id",
+      courierId: "courier-id",
+      isNewUser: false,
+      createdAt: new Date()
+    };
+    const documentedTransaction: Transaction = {
+      origin: {
+        id: "USA",
+        lat: 1,
+        lng: 0,
+        description: "-"
+      },
+      destination: {
+        id: "BCN",
+        lat: 1,
+        lng: 0,
+        description: "-"
+      },
+      transactionId: "transaction-id",
+      moneyAmount: 1000,
+      userId: "user-id",
+      courierId: "courier-id",
+      isNewUser: false,
+      createdAt: new Date()
+    };
+    expect(
+      TransactionService.isUndocumented(undocumentedTransaction)
+    ).toBeTruthy();
+    expect(
+      TransactionService.isUndocumented(documentedTransaction)
+    ).toBeFalsy();
+  });
+
+  it("should calculate discounted money amounts", () => {
+    expect(TransactionService.getDiscountedMoneyAmount(500)).toBe(500);
+    expect(TransactionService.getDiscountedMoneyAmount(2000)).toBe(
+      2000 - 0.1 * 2000
+    );
+    expect(TransactionService.getDiscountedMoneyAmount(5000)).toBe(
+      5000 - 0.2 * 5000
+    );
+    expect(TransactionService.getDiscountedMoneyAmount(9000)).toBe(
+      9000 - 0.3 * 9000
+    );
   });
 });
