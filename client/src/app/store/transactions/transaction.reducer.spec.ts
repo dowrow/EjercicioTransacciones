c1339dbc28d76b64405ee73690545791dcda2381
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2018-06-24 02:58:19 +0200

    WIP: Add toggle spec to trans. reducer

--- client/src/app/store/transactions/transaction.reducer.spec.ts
+++ client/src/app/store/transactions/transaction.reducer.spec.ts
@@ -7,7 +7,7 @@ import {
   TOGGLE_SHOW_UNDOCUMENTED
 } from "./transaction.actions";
 
-describe("RoleReducer", () => {
+describe("TransactionReducer", () => {
   describe("undefined action", () => {
     it("should return the default role", () => {
       const initialState = {
@@ -22,6 +22,7 @@ describe("RoleReducer", () => {
       expect(state).toBe(initialState);
     });
   });
+
   describe("TOGGLE_APPLY_DISCOUNTS action", () => {
     it("should toggle to true", () => {
       const initialState = {
@@ -33,14 +34,9 @@ describe("RoleReducer", () => {
       };
       const action = { type: TOGGLE_APPLY_DISCOUNTS };
       const state = transactionReducer(initialState, action);
-      expect(state).toBe({
-        page: 0,
-        transactions: [],
-        applyDiscounts: true,
-        showUndocumented: false,
-        markDuplicates: false
-      });
+      expect(state.applyDiscounts).toBeTruthy();
     });
+
     it("should toggle to false", () => {
       const initialState = {
         page: 0,
@@ -51,13 +47,63 @@ describe("RoleReducer", () => {
       };
       const action = { type: TOGGLE_APPLY_DISCOUNTS };
       const state = transactionReducer(initialState, action);
-      expect(state).toBe({
+      expect(state.applyDiscounts).toBeFalsy();
+    });
+  });
+
+  describe("TOGGLE_MARK_DUPLICATES action", () => {
+    it("should toggle to true", () => {
+      const initialState = {
         page: 0,
         transactions: [],
         applyDiscounts: false,
         showUndocumented: false,
         markDuplicates: false
-      });
+      };
+      const action = { type: TOGGLE_MARK_DUPLICATES };
+      const state = transactionReducer(initialState, action);
+      expect(state.markDuplicates).toBeTruthy();
+    });
+
+    it("should toggle to false", () => {
+      const initialState = {
+        page: 0,
+        transactions: [],
+        applyDiscounts: false,
+        showUndocumented: false,
+        markDuplicates: true
+      };
+      const action = { type: TOGGLE_MARK_DUPLICATES };
+      const state = transactionReducer(initialState, action);
+      expect(state.markDuplicates).toBeFalsy();
+    });
+  });
+
+  describe("TOGGLE_SHOW_UNDOCUMENTED action", () => {
+    it("should toggle to true", () => {
+      const initialState = {
+        page: 0,
+        transactions: [],
+        applyDiscounts: false,
+        showUndocumented: false,
+        markDuplicates: false
+      };
+      const action = { type: TOGGLE_SHOW_UNDOCUMENTED };
+      const state = transactionReducer(initialState, action);
+      expect(state.showUndocumented).toBeTruthy();
+    });
+
+    it("should toggle to false", () => {
+      const initialState = {
+        page: 0,
+        transactions: [],
+        applyDiscounts: false,
+        showUndocumented: true,
+        markDuplicates: false
+      };
+      const action = { type: TOGGLE_SHOW_UNDOCUMENTED };
+      const state = transactionReducer(initialState, action);
+      expect(state.showUndocumented).toBeFalsy();
     });
   });
 });
--- client/src/app/store/transactions/transaction.reducer.ts
+++ client/src/app/store/transactions/transaction.reducer.ts
@@ -1,4 +1,8 @@
 import { TransactionsModel } from "./transactions.model";
+import {
+  TOGGLE_APPLY_DISCOUNTS,
+  TOGGLE_MARK_DUPLICATES
+} from "./transaction.actions";
 
 const initialState: TransactionsModel = {
   page: 0,
@@ -10,6 +14,11 @@ const initialState: TransactionsModel = {
 
 export const transactionReducer = (state = initialState, action) => {
   switch (action.type) {
+    case TOGGLE_APPLY_DISCOUNTS:
+      return { ...state, applyDiscounts: !state.applyDiscounts };
+    case TOGGLE_MARK_DUPLICATES:
+      return { ...state, markDuplicates: !state.markDuplicates };
+
     default:
       return state;
   }
